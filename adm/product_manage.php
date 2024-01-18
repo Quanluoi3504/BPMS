e9072ef6037471a19d92550f30339a10bc702275
hkhanhdev <nguyenxuanhakhanh88060@gmail.com>
2024-01-18 11:18:33 +0700

    modify product page

--- adm/product_manage.php
+++ adm/product_manage.php
@@ -4,66 +4,34 @@ include_once ('database.php'); ?>
 <div id="content">
     <h1 style="font-weight: bold">Add product</h1>
     <table class="table table-hover">
-        <thead>
         <tr>
             <th scope="col">Product name</th>
             <th scope="col">Price</th>
             <th scope="col">Category</th>
             <th scope="col">Image</th>
-
+            <th scope="col">Color</th>
+            <th scope="col">Title</th>
+            <th scope="col">Material</th>
+            <th scope="col">Weight</th>
         </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td><input type="text" placeholder="Prd name" id="prd_name"></td>
-                <td>$<input type="text" placeholder="Prd price" id="prd_price"></td>
-                <td><input type="text" placeholder="Category" id="prd_cate"></td>
-                <td><input type="text" placeholder="Image file name" id="image_file_name"></td>
-                <td><button type="button" class="btn btn-outline-primary" onclick="add_prd()">Add</button></td>
-            </tr>
-<?php
-if (isset($_POST['prd_name']) && isset($_POST['prd_price']) && isset($_POST['prd_cate']) && isset($_POST['image'])) {
-    $prd_name = $_POST['prd_name'];
-    $prd_price = $_POST['prd_price'];
-    $prd_cate = $_POST['prd_cate'];
-    // Check if cate = macbook
-    if ($prd_cate == "Macbook") {
-        $is_macbook = true;
-    }else {
-        $is_macbook = false;
-    }
+        <tr>
+            <td><input type="text" placeholder="Prd name" id="prd_name"></td>
+            <td><input type="text" placeholder="$ Prd price" id="prd_price"></td>
+            <td><input type="text" placeholder="Category" id="prd_cate"></td>
+            <td><input type="text" placeholder="Image file name" id="image_file_name"></td>
+            <td><input type="text" placeholder="Color" id="color"></td>
+            <td><input type="text" placeholder="Title" id="title"></td>
+            <td><input type="text" placeholder="Material" id="material"></td>
+            <td><input type="text" placeholder="Weight" id="weight"></td>
+        </tr>
+        <tr>
+            <th scope="col">Size</th>
+        </tr>
+        <tr>
+            <td><input type="text" placeholder="Size" id="size"></td>
+            <td><button type="button" class="btn btn-outline-primary" onclick="add_prd()">Add</button></td>
+        </tr>
 
-    $img_file = $_POST['image'];
-    $check_if_cate_exist = "SELECT * FROM tbl_category WHERE cate_name = '$prd_cate'";
-    $if_cate_exist = mysqli_query($conn, $check_if_cate_exist);
-    // Check if cate already existed then just add new product, if not add new cate then add new product
-    if ($if_cate_exist->num_rows == 0) {
-        $insert_new_cate = "INSERT INTO tbl_category(cate_name) VALUES ('$prd_cate')";
-        $inserted_new_cate = mysqli_query($conn, $insert_new_cate);
-        $cate_now_exist = mysqli_query($conn, $check_if_cate_exist);
-        $cate_row = $cate_now_exist ->fetch_assoc();
-        if($is_macbook) {
-            $insert_prd = "INSERT INTO tbl_product(prd_name, prd_price, prd_image, cate_id,prd_ram,prd_gpu,prd_resolution,prd_operating_system) 
-               VALUES ('$prd_name', '$prd_price','$img_file',{$cate_row['cate_id']},'64GB','M1 Pro Max','2560 x 1600 pixels (2K)','MacOS')";
-        }else {
-            $insert_prd = "INSERT INTO tbl_product(prd_name, prd_price, prd_image, cate_id,prd_ram,prd_gpu,prd_resolution,prd_operating_system) 
-               VALUES ('$prd_name', '$prd_price','$img_file',{$cate_row['cate_id']},'64GB','NVIDIA GeForce RTX 4090 Ti','1920 x 1080 pixels (FullHD)','Windows')";
-        }
-        $inserted_prd = mysqli_query($conn, $insert_prd);
-    }else {
-        $cate_row = $if_cate_exist->fetch_assoc();
-        if($is_macbook) {
-            $insert_prd = "INSERT INTO tbl_product(prd_name, prd_price, prd_image, cate_id,prd_ram,prd_gpu,prd_resolution,prd_operating_system) 
-               VALUES ('$prd_name', '$prd_price','$img_file',{$cate_row['cate_id']},'64GB','M1 Pro Max','2560 x 1600 pixels (2K)','MacOS')";
-        }else {
-            $insert_prd = "INSERT INTO tbl_product(prd_name, prd_price, prd_image, cate_id,prd_ram,prd_gpu,prd_resolution,prd_operating_system) 
-               VALUES ('$prd_name', '$prd_price','$img_file',{$cate_row['cate_id']},'64GB','NVIDIA GeForce RTX 4090 Ti','1920 x 1080 pixels (FullHD)','Windows')";
-        }
-        $inserted_prd = mysqli_query($conn, $insert_prd);
-    }
-}
-?>
-        </tbody>
     </table>
     <br>
     <h1 style="font-weight: bold">All products</h1>
@@ -111,12 +79,52 @@ if ($result->num_rows > 0) {
     </table>
 </div>
 
+<?php
+if (isset($_POST['prd_name']) && isset($_POST['prd_price']) && isset($_POST['prd_cate']) && isset($_POST['image'])) {
+    $prd_name = $_POST['prd_name'];
+    $prd_price = $_POST['prd_price'];
+    $prd_cate = $_POST['prd_cate'];
+    $img_file = $_POST['image'];
+    $color = $_POST['color'];
+    $title = $_POST['title'];
+    $material = $_POST['material'];
+    $weight = $_POST['weight'];
+    $size = $_POST['size'];
+
+    $check_if_cate_exist = "SELECT * FROM tbl_category WHERE cate_name = '$prd_cate'";
+    $if_cate_exist = mysqli_query($conn, $check_if_cate_exist);
+    // Check if cate already existed then just add new product, if not add new cate then add new product
+    if ($if_cate_exist->num_rows == 0) {
+        $insert_new_cate = "INSERT INTO tbl_category(cate_name) VALUES ('$prd_cate')";
+        $inserted_new_cate = mysqli_query($conn, $insert_new_cate);
+        $cate_now_exist = mysqli_query($conn, $check_if_cate_exist);
+        $cate_row = $cate_now_exist ->fetch_assoc();
+
+        $insert_prd = "INSERT INTO tbl_product(prd_name, prd_price, prd_image, cate_id,prd_color,prd_title,prd_material,prd_weight,prd_size) 
+           VALUES ('$prd_name', '$prd_price','$img_file',{$cate_row['cate_id']},'$color','$title','$material','$weight','$size')";
+
+        $inserted_prd = mysqli_query($conn, $insert_prd);
+    }else {
+        $cate_row = $if_cate_exist->fetch_assoc();
+
+        $insert_prd = "INSERT INTO tbl_product(prd_name, prd_price, prd_image, cate_id,prd_color,prd_title,prd_material,prd_weight,prd_size) 
+           VALUES ('$prd_name', '$prd_price','$img_file',{$cate_row['cate_id']},'$color','$title','$material','$weight','$size')";
+
+        $inserted_prd = mysqli_query($conn, $insert_prd);
+    }
+}
+?>
 <script>
     function add_prd() {
         var prd_name = document.getElementById('prd_name').value;
         var prd_price = document.getElementById('prd_price').value;
         var prd_cate = document.getElementById('prd_cate').value;
         var image_file_name = document.getElementById('image_file_name').value;
+        var color = document.getElementById('color').value;
+        var title = document.getElementById('title').value;
+        var material = document.getElementById('material').value;
+        var weight = document.getElementById('weight').value;
+        var size = document.getElementById('size').value;
         // Make AJAX request to the PHP file
         var xhr = new XMLHttpRequest();
         xhr.open('POST', window.location.href, true);
@@ -126,6 +134,11 @@ if ($result->num_rows > 0) {
         formData.append('prd_price',prd_price);
         formData.append('prd_cate',prd_cate);
         formData.append('image',image_file_name);
+        formData.append('color', color);
+        formData.append('title',title);
+        formData.append('material',material);
+        formData.append('weight',weight);
+        formData.append('size',size);
         xhr.onreadystatechange = function() {
             if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
                 alert("Adding product to database ...")

c4a88d9767c0bc0b09748f1b086f38722aac1f61
hkhanhdev <nguyenxuanhakhanh88060@gmail.com>
2024-01-18 10:29:46 +0700

    add logo for admin page

--- adm/topbar_sidebar.php
+++ adm/topbar_sidebar.php
@@ -2,8 +2,8 @@
 <html lang="en">
 <head>
     <meta charset="utf8">
-    <title>LK Admin Dashboard</title>
-    <link rel="icon" href="images/Lâm%20and%20Khánh%20(1).png" type="image/png"/>
+    <title>LKQ Admin Dashboard</title>
+    <link rel="icon" href="images/logoo.png" type="image/png"/>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css" />
     <link rel="stylesheet" href="css/responsive.css" />
@@ -36,7 +36,7 @@
                     <li><a href="dashboard.php"> <span>Dashboard</span></a></li>
                     <li><a href="product_manage.php"><span>Products Management</span></a></li>
                     <li><a href="user_manage.php"> <span>Users Management</span></a></li>
-                    <li><a href="order_manage.php"> <span>Orders Management</span></a></li>
+
                 </ul>
             </div>
         </nav>
@@ -44,7 +44,7 @@
         <div class="topbar">
             <nav class="navbar navbar-expand-lg navbar-light">
                 <div class="logo_section">
-                    <a href="../index.php"><img class="img-responsive" src="images/Lâm%20and%20Khánh%20(1).png" alt="#" /></a>
+                    <a href="../index.php"><img class="img-responsive" src="images/logoo.png" alt="#" /></a>
                 </div>
             </nav>
         </div>

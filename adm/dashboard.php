60fb533b7661d02f778370ee00101c41f9dd1454
hkhanhdev <nguyenxuanhakhanh88060@gmail.com>
2024-01-18 10:42:59 +0700

    modify dashboard page

--- adm/dashboard.php
+++ adm/dashboard.php
@@ -6,11 +6,8 @@ include_once ('database.php');
 <?php
 $user_sql = "select count(user_id) as so_luong_nguoi_dung from tbl_user;";
 $prd_sql = "select count(prd_id) as so_luong_san_pham from tbl_product;";
-$ord_sql = "select count(ord_id) as so_luong_don_hang from tbl_order;";
 $user_result = mysqli_query($conn,$user_sql);
 $prd_result = mysqli_query($conn,$prd_sql);
-$ord_result = mysqli_query($conn,$ord_sql);
-
 ?>
 <div id="content">
     <div class="midde_cont">
@@ -39,16 +36,6 @@ $ord_result = mysqli_query($conn,$ord_sql);
                          <div>
                             <p class="total_no"><?php $row=$prd_result->fetch_assoc();echo $row['so_luong_san_pham']?></p>
                             <p class="head_couter">Products</p>
-                         </div>
-                      </div>
-                   </div>
-                </div>
-                <div class="col-md-6 col-lg-3">
-                   <div class="full counter_section">
-                       <div class="counter_no">
-                         <div>
-                            <p class="total_no"><?php $row=$ord_result->fetch_assoc();echo $row['so_luong_don_hang']?></p>
-                            <p class="head_couter">Orders</p>
                          </div>
                       </div>
                    </div>
